04bba870d40c70654b0147e0fefc9ae1468fc020
Umut Tekinışık <tekinisikumut@gmail.com>
2026-02-02 13:36:08 +0300

    Update solution for dot-product

--- dot-product/dot-product.py
+++ dot-product/dot-product.py
@@ -5,5 +5,6 @@ def dot_product(x, y):
     Compute the dot product of two 1D arrays x and y.
     Must return a float.
     """
+    # return float(np.sum(np.multiply(x, y)))
 
     return float(np.vecdot(x, y))
